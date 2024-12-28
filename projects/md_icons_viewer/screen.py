65b7eb3d3a539a0d683cf22bb99ec719795e3472
Sebastian Pacurar <pacurar.sebastian90@gmail.com>
2024-12-28 18:11:56 +0200

    added compact vs expanded grid view for MdIconsViewerScreen  - based on right button_container_right of toolbar  - added toggle function to change icons to no icon_name and small outlayer vs large outlayer with icon_name displayed  - fixed some TopBar esthetics

--- projects/md_icons_viewer/screen.py
+++ projects/md_icons_viewer/screen.py
@@ -1,5 +1,6 @@
 from kivy.app import App
-from kivy.properties import StringProperty, ListProperty, NumericProperty
+from kivy.metrics import dp, sp
+from kivy.properties import StringProperty, ListProperty, NumericProperty, BooleanProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import Screen
 from assets.fonts.material_design.webfont_unicodes import icons
@@ -10,23 +11,20 @@ class MdIconsViewerScreen(Screen):
     original_data = ListProperty()  # unfiltered complete data
     filtered_data = ListProperty()  # data subset based on toggle buttons
     counter = NumericProperty(0)  # count of currently filtered options
+    is_compact = BooleanProperty(False)  # used to switch between small icons and large icons
 
     def set_data(self):
-        # convert icons dictionary to a list of dictionaries for RecycleView
-        self.original_data = [{'icon': v, 'icon_name': k} for k, v in icons.items()]
-        self.filtered_data = self.original_data.copy()  # start with all items
+        # Convert icons dictionary to a list of dictionaries for RecycleView
+        self.original_data = [{'icon': v, 'icon_name': k, 'is_name_displayed': not self.is_compact} for k, v in icons.items()]
+        self.filtered_data = self.original_data.copy()  # Start with all items
         self.data = self.filtered_data.copy()
         self.counter = len(self.data)
 
     def on_kv_post(self, base_widget):
         top_bar = self.ids.top_bar
-        top_bar.add_right_button(icon=App.get_running_app().get_icon('grid'), on_release=self.test_hello)
-        top_bar.add_right_button(icon=App.get_running_app().get_icon('grid-large'), on_release= self.test_hello)
+        top_bar.add_right_button(icon=App.get_running_app().get_icon('grid'), on_release=self.toggle_grid_display_size)
         self.set_data()
         self.ids.responsive_grid.ids.rv.data = self.data
-
-    def test_hello(self, *args):
-        print('test')
 
     def set_filter_selection(self, toggle_value):
         """ Update the filtered_data based on toggle button selection """
@@ -47,13 +45,43 @@ class MdIconsViewerScreen(Screen):
 
         else:  # reset data if text is empty
             self.data = self.filtered_data.copy()
+
         self.counter = len(self.data)
+        self.refresh_recycle_view()
+
+    def toggle_grid_display_size(self, *args):
+        self.is_compact = not self.is_compact
+
+        self.ids.responsive_grid.item_width = dp(200) if not self.is_compact else dp(75)
+        for lst in [self.data, self.filtered_data, self.original_data]:
+            for item in lst:
+                item['is_name_displayed'] = not self.is_compact
+
+        if self.is_compact:
+            self.ids.top_bar.ids.button_container_right.children[0].icon = App.get_running_app().get_icon('grid-large')
+        else:
+            self.ids.top_bar.ids.button_container_right.children[0].icon = App.get_running_app().get_icon('grid')
+
+        self.refresh_recycle_view()
+
+    def refresh_recycle_view(self):
         self.ids.responsive_grid.ids.rv.data = self.data
 
 
 class IconItem(FloatLayout):
     icon = StringProperty('')  # icon unicode
     icon_name = StringProperty('')  # icon name
+    is_name_displayed = BooleanProperty(True)  # start with large grid
 
     def on_icon(self, instance, value):
         self.ids.icon_label.text = value
+
+    def on_is_name_displayed(self, instance, value):
+        if value:
+            self.ids.icon_label.pos_hint = {'center_x': 0.5, 'center_y': 0.7}
+            self.ids.icon_label.font_size = sp(52)
+            self.ids.icon_name.opacity = 1
+        else:
+            self.ids.icon_name.opacity = 0
+            self.ids.icon_label.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+            self.ids.icon_label.font_size = sp(34)
