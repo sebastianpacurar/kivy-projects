601f6215a91baae5db6150fe5dda20281c04e200
Sebastian Pacurar <pacurar.sebastian90@gmail.com>
2025-01-23 00:29:47 +0200

    customize ColorPickerWidget:  - handle only rgb values, through sliders separately from ColorPicker class  - use only ColorWheel Widget as extension   - override on_touch_move, in favor of 2 buttons to increase/decrease brightness  - improve color display with value

--- projects/color_picker/screen.py
+++ projects/color_picker/screen.py
@@ -1,5 +1,6 @@
-from kivy.properties import ObjectProperty
-from kivy.uix.colorpicker import ColorPicker
+from kivy.properties import BoundedNumericProperty, BooleanProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.colorpicker import ColorWheel
 from kivy.uix.screenmanager import Screen
 
 
@@ -7,14 +8,54 @@ class ColorPickerScreen(Screen):
     pass
 
 
-class CustomColorPicker(ColorPicker):
-    color_label = ObjectProperty(None)
-    color_preview = ObjectProperty(None)
+class ColorPickerWidget(BoxLayout):
+    red = BoundedNumericProperty(0, min=0, max=255)
+    green = BoundedNumericProperty(0, min=0, max=255)
+    blue = BoundedNumericProperty(0, min=0, max=255)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_kv_post(self, base_widget):
+        self.ids.color_wheel.bind(on_color=self.set_color_from_wheel)
+
+    def on_slider_value_change(self, color, value):
+        setattr(self, color, int(value))
+
+    def set_color_from_wheel(self, value):
+        for i, color in enumerate([self.ids.red_slider, self.ids.green_slider, self.ids.blue_slider]):
+            color.value = int(value[i] * 255)
+
+
+class ColorWheelWidget(ColorWheel):
+    can_lighten = BooleanProperty(False)  # start from max light
+    can_darken = BooleanProperty(True)
 
     def on_color(self, instance, value):
-        r, g, b, _ = value
-        self.color_label.text = f"RGB: ({int(r * 255)}, {int(g * 255)}, {int(b * 255)})"
+        self.parent.parent.parent.parent.set_color_from_wheel(value)
 
-    def set_color(self, color):
-        print(self.children[0].children[1].children[0].canvas.children[1])
-        super().set_color(color)
+    def update_lighten_darken_states(self, *args):
+        self.can_lighten = self.sv_idx > 0
+        self.can_darken = self.sv_idx < len(self.sv_s) - self._piece_divisions
+
+    def lighten(self):
+        """ Increase the brightness by decreasing  \n
+            Same functionality as on_touch_move outwards the center
+        """
+        if self.can_lighten:
+            self.sv_idx -= 1
+            self.recolor_wheel()
+            self.update_lighten_darken_states()
+
+    def darken(self):
+        """ Decrease the brightness by moving sv_idx to a darker value \n
+            Same functionality as on_touch_move towards the center
+        """
+        if self.can_darken:
+            self.sv_idx += 1
+            self.recolor_wheel()
+            self.update_lighten_darken_states()
+
+    # cancel on_touch_move, since lighten and darken already take care of that
+    def on_touch_move(self, touch):
+        return False
