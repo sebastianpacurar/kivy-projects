946f11ff2baa96428908486c73b929e46043e35f
Sebastian Pacurar <pacurar.sebastian90@gmail.com>
2025-01-24 16:55:17 +0200

    Refactor Tooltip custom widget:  - isolate hide/show logic in the Tooltip class    - using a tracker, for scheduling a tooltip checking periodically    - if tracker started, then the function is scheduled at 60 fps. If stopped, then cancel the interval - improve MDIconsViewerScreen search functionality to split icon names by dashes

--- custom_widgets/Tooltip/tooltip.py
+++ custom_widgets/Tooltip/tooltip.py
@@ -1,10 +1,16 @@
 from kivy.core.window import Window
 from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
+from kivy.clock import Clock
 
 
 class Tooltip(FloatLayout):
     tooltip_text = StringProperty('')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.target_widget = None  # widget which will render the tooltip
+        self.tooltip_scheduled_event = None  # the actual clocked event, in this case a scheduled interval
 
     def show_tooltip(self, pos):
         """ Calculate the tooltip to properly fit in the screen without spilling out \n
@@ -15,8 +21,8 @@ class Tooltip(FloatLayout):
         screen_width, screen_height = Window.size
 
         # default position (below the mouse, starts from the mouse towards right side)
-        tooltip_x = pos[0]  # position on the right side of the mouse
-        tooltip_y = pos[1] - tooltip_height - 10  # position below the mouse
+        tooltip_x = pos[0]  # Position on the right side of the mouse
+        tooltip_y = pos[1] - tooltip_height - 10  # Position below the mouse
 
         # no spillout on the right side (flip to display towards left side of the mouse)
         if tooltip_x + tooltip_width > screen_width:
@@ -26,12 +32,51 @@ class Tooltip(FloatLayout):
         if tooltip_y < 0:
             tooltip_y = pos[1] + 10
 
-        self.pos = (tooltip_x, tooltip_y)
+        # update position only if different from tooltip pos
+        if self.pos != (tooltip_x, tooltip_y):
+            self.pos = (tooltip_x, tooltip_y)
 
         # add widget if there is no widget to Window tree
         if self not in Window.children:
             Window.add_widget(self)
 
     def hide_tooltip(self):
-        """ Remove Tooltip widget from Window tree"""
-        Window.remove_widget(self)
+        """ Remove Tooltip widget from Window tree """
+        if self in Window.children:
+            Window.remove_widget(self)
+
+    def start_tracking(self, target_widget, parent=None):
+        """ Start tracking a target widget for tooltip display \n
+            This should be used as a trigger for when the tooltip should be tracked under certain conditions
+        """
+        self.target_widget = target_widget
+        if self.tooltip_scheduled_event is None:
+            self.tooltip_scheduled_event = Clock.schedule_interval(
+                lambda dt: self.check_tooltip_periodically(dt, target_widget.parent), 1 / 60
+            )
+
+    def stop_tracking(self):
+        """ Stop tracking the target widget for tooltip display \n
+            This should be used as a trigger for when the start_tracking() condition is not respected anymore
+        """
+        if self.tooltip_scheduled_event:
+            self.tooltip_scheduled_event.cancel()
+            self.tooltip_scheduled_event = None
+        self.target_widget = None
+        self.hide_tooltip()
+
+    def check_tooltip_periodically(self, dt, parent=None):
+        """ Check if the tooltip should be shown, hidden, or repositioned """
+        if not self.target_widget:
+            return
+
+        if parent is not None:  # using this in case the widget is part of a RV data, and is not currently visible in the viewport
+            pos = Window.mouse_pos  # get the current mouse position on Window
+            if self.target_widget.collide_point(*self.target_widget.to_widget(*pos)):
+                # show tooltip if mouse overlaps the target widget
+                self.show_tooltip((pos[0] + 10, pos[1] - 10))
+            else:
+                # hide tooltip if mouse is not over the target widget
+                self.hide_tooltip()
+        else:
+            self.hide_tooltip()
