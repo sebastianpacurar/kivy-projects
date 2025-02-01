ffead192ca513a66ebaf6d1dbbdf6aa6b4da9903
Sebastian Pacurar <pacurar.sebastian90@gmail.com>
2025-02-01 15:58:20 +0200

    implemented update events on ColorBox:  - clicking or holding click while moving the mouse over the ColorTable, triggers update event for both color_display and sliders  - store all ColorBox labels in the form of {(rgb_list):ColorBoxWidget} and set updates both ways:   - when matching a ColorBox bg_color value while from sliders   - when clicking a ColorBox, and updating the sliders and color_display layout  - some fixes on SliderWidget  - found bug on color_display color setup with rgb_format from utils

--- custom_widgets/ColorPickerWidget/color_picker_widget.py
+++ custom_widgets/ColorPickerWidget/color_picker_widget.py
@@ -1,5 +1,6 @@
+from kivy.graphics import Color, Line
 from kivy.metrics import dp
-from kivy.properties import BoundedNumericProperty, NumericProperty, StringProperty, ReferenceListProperty
+from kivy.properties import BoundedNumericProperty, NumericProperty, StringProperty, ReferenceListProperty, ColorProperty, ListProperty, DictProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 
@@ -16,19 +17,37 @@ class ColorPickerWidget(BoxLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.focused_color_box = None  # currently selected ColorBox from the ColorTable
 
     def on_kv_post(self, base_widget):
-        self.bind(rgb_colors=self.rgb_to_hex)
+        self.bind(rgb_colors=self.rgb_to_hex)  # convert to hex when any of r,g or b changes
+        # start from purple-like color
+        self.red = 104
+        self.green = 4
+        self.blue = 150
 
     def update_color_channel(self, instance, value):
         setattr(self, instance.parent.target_prop_attribute, int(value))
 
-    def set_color_from_table(self, value):
+    def set_slider_vals_from_table(self, value):
         for i, s in enumerate([self.ids.red_slider, self.ids.green_slider, self.ids.blue_slider]):
             s.ids.slider_w.value = int(value[i] * 255)
 
     def rgb_to_hex(self, instance, value):
         self.hex_val = '#{:02X}{:02X}{:02X}'.format(self.red, self.green, self.blue)
+
+    # update the focused_color_box transparency value when rgb_colors change
+    def on_rgb_colors(self, instance, value):
+        val = tuple(value)
+        # set opacity to 0 for any displayed ColorBox border and clear the focused_color_box ref
+        if self.focused_color_box is not None:
+            self.focused_color_box.color_box_selection_color[-1] = 0
+            self.focused_color_box = None
+
+        # if the rgb val matches any ColorBox bg_color from the color_table, then focus it and set opacity to 1
+        if val in self.color_table.color_boxes:
+            self.focused_color_box = self.color_table.color_boxes[val]
+            self.focused_color_box.color_box_selection_color[-1] = 1
 
     def hex_to_rgb(self, instance, value):
         hex_stripped = value[1:]
@@ -37,6 +56,8 @@ class ColorPickerWidget(BoxLayout):
 
 
 class ColorTable(BoxLayout):
+    color_boxes = DictProperty({})  # store all color BaseLabel widgets, in this form {rgb_value: widget}
+
     def on_kv_post(self, touch):
         lightened = ColorGrid(orientation='rl-tb')
         darkened = ColorGrid(orientation='lr-tb')
@@ -44,6 +65,7 @@ class ColorTable(BoxLayout):
         self.add_widget(darkened)
         self.height = lightened.height
         self.width = lightened.width + darkened.width
+        self.color_boxes = {**lightened.color_boxes, **darkened.color_boxes}
 
 
 class ColorGrid(GridLayout):
@@ -52,6 +74,7 @@ class ColorGrid(GridLayout):
     cell_size = NumericProperty(dp(10))
     min_b = NumericProperty(0.1)
     max_b = NumericProperty(1.0)
+    color_boxes = DictProperty({})
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -87,7 +110,7 @@ class ColorGrid(GridLayout):
                     self.is_last = row == 0
                     self.is_first = row == self.rows - 1
 
-                # hide first and last columns for left grid, and hide last row for right grid
+                # hide first and last columns for left grid, and hide last column (from rght to left) for right grid
                 condition = False
                 if self.is_last is not None:
                     condition = self.is_last and self.is_last
@@ -98,32 +121,50 @@ class ColorGrid(GridLayout):
                     bg_color=[r_bright, g_bright, b_bright, 1],
                     size=(self.cell_size, self.cell_size),
                     opacity=0 if condition else 1,
-                    disabled=1 if condition else 0
+                    disabled=1 if condition else 0,
+                    color_box_selection_color=[1, 1, 1, 0] if self.orientation == 'lr-tb' else [0, 0, 0, 0]
                 )
                 self.add_widget(color_box)
+                # add entry as rgb:widget => {(255, 255, 255):ColorBox}
+                self.color_boxes[tuple([int(i * 255) for i in color_box.bg_color[:-1]])] = color_box
 
 
 class ColorBox(BaseLabel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.size_hint = None, None
+    color_box_selection_color = ColorProperty([1, 1, 1, 0])
 
+    def on_kv_post(self, base_widget):
+        self.bind(size=self.draw_color_borders, pos=self.draw_color_borders, color_box_selection_color=self.draw_color_borders)
+
+    # update sliders with bg_color value when click on ColorBox
     def on_touch_down(self, touch):
         if not self.disabled:
             if self.collide_point(*touch.pos):
                 self.set_sliders_color()
-        return super().on_touch_up(touch)
+        return super().on_touch_down(touch)
 
+    # update sliders with bg_color value when click + move over the table
     def on_touch_move(self, touch):
         if not self.disabled:
             if self.collide_point(*touch.pos):
                 self.set_sliders_color()
         return super().on_touch_move(touch)
 
+    # keep looking for set_slider_vals_from_table method by traversing parents, then apply this ColorBox bg_color value
     def set_sliders_color(self):
         parent = self.parent
         while parent:
-            if hasattr(parent, 'set_color_from_table'):
-                parent.set_color_from_table(self.bg_color)
+            if hasattr(parent, 'set_slider_vals_from_table'):
+                parent.set_slider_vals_from_table(self.bg_color)
                 break
             parent = parent.parent
+
+    def draw_color_borders(self, *args):
+        if self.canvas is None:
+            return
+        self.canvas.after.clear()
+        with self.canvas.after:
+            Color(*self.color_box_selection_color)
+            Line(
+                width=dp(.75),
+                rectangle=(self.x, self.y, self.width - dp(1), self.height - dp(1))
+            )
--- custom_widgets/SliderWidget/slider_widget.py
+++ custom_widgets/SliderWidget/slider_widget.py
@@ -1,20 +1,21 @@
 from kivy.metrics import dp
-from kivy.properties import NumericProperty, StringProperty, ColorProperty, ObjectProperty
+from kivy.properties import NumericProperty, StringProperty, ColorProperty, ObjectProperty, ListProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 
 
 class SliderWidget(BoxLayout):
     """ target_prop_attribute = the kivy property which will hold the value \n
         slider_value_func = the callback for when the value changes \n
+        is_int = use int value representation. if False it uses float
     """
     label_text = StringProperty('')
     target_prop_attribute = StringProperty('')
     slider_height = NumericProperty(dp(30))
-    current_value = NumericProperty(0)
     track_color = ColorProperty([1, 1, 1, 1])
-    min_val = NumericProperty(0)
-    max_val = NumericProperty(100)
+    min_max = ListProperty([0, 100])
     slider_value_func = ObjectProperty(None)
+    default_value = NumericProperty(0)
+    is_int = BooleanProperty(True)
 
     def on_slider_value_func(self, instance, value):
         self.ids.slider_w.bind(value=self.slider_value_func)
